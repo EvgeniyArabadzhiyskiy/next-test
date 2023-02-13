1644794133f05bb342092883b81c5a426fb2a21a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-13 22:00:05 +0200

    Create pokemonApi.js

---
+++ redux/pokemonApi.js
@@ -0,0 +1,56 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { HYDRATE } from "next-redux-wrapper";
+
+export const pokemonApi = createApi({
+  reducerPath: "pokemonApi",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://pokeapi.co/api/v2/",
+  }),
+
+  extractRehydrationInfo(action, { reducerPath }) {
+    if (action.type === HYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
+
+  tagTypes: ["Pokemon"],
+
+  endpoints: (builder) => ({
+    getPokemonByName: builder.query({
+      query: (name) => `pokemon/${name}`,
+
+      transformResponse: (response) => {
+        return {
+          species: response.species,
+          sprites: response.sprites,
+        };
+      },
+    }),
+
+    providesTags: ["Pokemon"],
+
+    getPokemonList: builder.query({
+      query: (item) => {
+        console.log("item", item);
+        // console.log("World");
+
+        return `pokemon?limit=10`;
+      },
+
+      transformResponse: (response) => response.results,
+    }),
+
+    providesTags: ["Pokemon"],
+  }),
+});
+
+// Export hooks for usage in functional components
+export const {
+  useGetPokemonByNameQuery,
+  useGetPokemonListQuery,
+  util: { getRunningQueriesThunk },
+} = pokemonApi;
+
+// export endpoints for use in SSR
+export const { getPokemonByName, getPokemonList } = pokemonApi.endpoints;
