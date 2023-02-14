c61f386c1144ad28aef574d976bc2c3a9ee38ff6
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-14 22:01:52 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,23 +2,41 @@ import { NextResponse } from "next/server";
 import { makeStore } from "./redux/store";
 
 export function middleware(request) {
-  const store = makeStore();
-  const { pathname } = request.nextUrl
+  // console.log(global.localStorage);
 
-  const { isLoggedIn } = store.getState().auth;
 
-  if (!isLoggedIn) {
-    return NextResponse.redirect(new URL("/pokemons/statistic", request.url));
-  }
+
+  const cookies = request.cookies
+  // console.log("middleware  cookies", !!cookies.get('goit')?.value);
+  
+
+  const response = NextResponse.next();
+  // response.cookies.set('age', 35)
+
+  // const cookies = request.cookies
+  // console.log("middleware  cookies", cookies.get('promo')?.value);
+
+  // const store = makeStore();
+  // const { counter } = store.getState().counter;
+  // console.log("middleware  =================counter", counter);
+
+  // const { pathname } = request.nextUrl
+
+  // const { isLoggedIn } = store.getState().auth;
+  // console.log("middleware  isLoggedIn", isLoggedIn);
+
+  // if (!isLoggedIn) {
+  //   return NextResponse.redirect(new URL("/pokemons/statistic", request.url));
+  // }
 
   // if ( pathname === '/blog') {
   //   return NextResponse.redirect(new URL("/pokemons/raiting", request.url));
   // }
 
   //   const response = NextResponse.next({ //config })
-  //   return  response
+  return response;
 }
 
-export const config = {
-  matcher: ['/blog', "/pokemons"],
-};
+// export const config = {
+//   matcher: ['/blog', "/pokemons"],
+// };
