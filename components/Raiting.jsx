2c66d28738f7749b8fa1528411af072c60d16c14
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-16 21:44:30 +0200

    Create Raiting.jsx

---
+++ components/Raiting.jsx
@@ -0,0 +1,17 @@
+const Raiting = () => {
+  return (
+    <div>
+      <h1>Pokemon Raiting</h1>
+
+      <div className="inner"></div>
+      <style jsx>{`
+        .inner {
+          height: 200px;
+          background: tomato;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Raiting;
