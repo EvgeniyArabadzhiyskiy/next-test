8f5313e2a756ca837a45c49ec6e945d96e313221
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-16 21:44:47 +0200

    Create Person.jsx

---
+++ components/Person.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function PersonComponent({ person }) {
+  return (
+    <li>
+      <Link href="/person/[id]" as={`/person/${person.id}`}>
+        {person.name}
+      </Link>
+    </li>
+  );
+}
