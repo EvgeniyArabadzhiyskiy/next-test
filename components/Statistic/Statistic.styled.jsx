f0b80a9848a82a19011da7dbb04a46d818396d3c
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-17 21:51:15 +0200

    Create Statistic.styled.jsx

---
+++ components/Statistic/Statistic.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 30px;
+  color: #0674d4;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 200px;
+  background: #11b843;
+`;
