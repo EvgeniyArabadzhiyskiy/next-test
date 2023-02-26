e93d1ad908c7e67eb52ef61ed02faac150c84e70
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-26 18:21:59 +0200

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,28 +1,75 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { createWrapper } from "next-redux-wrapper";
-import { authReducer } from "./auth/authSlice";
-import { counterReduser } from "./counter/counter";
-import { pokemonApi } from "./pokemonApi";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { createWrapper, HYDRATE } from "next-redux-wrapper";
+
 import { transactionReducer } from "./transactions-slice";
+import { counterReduser } from "./counter/counter";
+import { authReducer } from "./auth/authSlice";
+import { pokemonApi } from "./pokemonApi";
 import { walletApi } from "./walletApi";
 
+const rootReducer = combineReducers({
+  auth: authReducer,
+  counter: counterReduser,
+  transactions: transactionReducer,
+  [walletApi.reducerPath]: walletApi.reducer,
+  [pokemonApi.reducerPath]: pokemonApi.reducer,
+});
 
-export const makeStore = () =>
-  configureStore({
-    reducer: {
-      [pokemonApi.reducerPath]: pokemonApi.reducer,
-      [walletApi.reducerPath]: walletApi.reducer,
-      transactions: transactionReducer,
+const reducerSSR = (state, action) => {
+  if (action.type === HYDRATE) {
+    let nextState = {
+      ...state,
+      ...action.payload,
+    };
 
-      counter: counterReduser,
-      auth: authReducer,
-    },
+    return nextState;
+  } else {
+    return rootReducer(state, action);
+  }
+};
+
+export const makeStore = () => {
+  let middleware = [pokemonApi.middleware, walletApi.middleware];
+
+  const store = configureStore({
+    reducer: reducerSSR,
+    // middleware: (gDM) => gDM().concat(middleware),
 
     middleware: (gDM) => gDM().concat(pokemonApi.middleware, walletApi.middleware),
   });
 
+  return store;
+};
+
 export const wrapper = createWrapper(makeStore);
 
+//===============================================================
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import { createWrapper } from "next-redux-wrapper";
+// import { counterReduser } from "./counter/counter";
+// import { authReducer } from "./auth/authSlice";
+// import { pokemonApi } from "./pokemonApi";
+// import { transactionReducer } from "./transactions-slice";
+// import { walletApi } from "./walletApi";
+
+// export const makeStore = () =>
+//   configureStore({
+//     reducer: {
+//       [pokemonApi.reducerPath]: pokemonApi.reducer,
+//       [walletApi.reducerPath]: walletApi.reducer,
+//       transactions: transactionReducer,
+
+//       counter: counterReduser,
+//       auth: authReducer,
+//     },
+
+//     middleware: (gDM) => gDM().concat(pokemonApi.middleware, walletApi.middleware),
+//   });
+
+// export const wrapper = createWrapper(makeStore);
+
+//===========================================================
 
 // export type AppStore = ReturnType<typeof makeStore>;
 // export type RootState = ReturnType<AppStore["getState"]>;
