94a06fe9f0d9369affd47d43f7c626f54da59cca
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-27 20:11:15 +0200

    Update raiting.jsx

--- pages/pokemons/raiting.jsx
+++ pages/pokemons/raiting.jsx
@@ -1,8 +1,25 @@
+import Counter from "@/components/Counter/Counter";
 import PokemonPageLayout from "@/components/PokemonPageLayout";
 import Raiting from "@/components/Raiting";
+import { incrementCounter } from "@/redux/counter/counter";
+import { wrapper } from "@/redux/store";
+import { useSelector } from "react-redux";
+
+// export const getStaticProps = wrapper.getStaticProps(
+//   (store) => async () => {
+
+    
+
+//     store.dispatch(incrementCounter());
+
+//     return { props: {} };
+//   }
+// );
 
 const RaitingPage = () => {
-    return ( <Raiting /> );
+  const { counter } = useSelector(state => state.counter);
+    // console.log("RaitingPage  counter:", counter);
+    return ( <><Raiting /><Counter /></> );
 }
  
 export default RaitingPage;
