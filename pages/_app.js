affd4fc783f8cc3e649bae3b213827586c4ece51
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-04 20:17:58 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,74 +1,56 @@
-import GlobalLayout from "@/components/GlobalLayout";
-// import "@/styles/globals.css";
-import { GlobalStyles } from "@/styles/globalStyles";
-// import './_app.css'
-import AbortController from "abort-controller";
-import { Provider } from "react-redux";
+// import GlobalLayout from "@/components/GlobalLayout";
+// // import "@/styles/globals.css";
+// import { GlobalStyles } from "@/styles/globalStyles";
+// // import './_app.css'
+// import AbortController from "abort-controller";
+// import { Provider } from "react-redux";
 
-import App from "next/app";
-import { incrementCounter, setInitialCounter } from "@/redux/counter/counter";
-import { setInitialTransactions } from "@/redux/transactions-slice";
+// globalThis.AbortController = AbortController;
+// // import { wrapper } from "@/redux/store";     // НЕ РАБОТАЕТ С ТАКИМ IMPORT
+// const { wrapper } = require("../redux/store"); //  ТОЛЬКО ТАКОЙ IMPORT И ТОЛЬКО  после AbortController
 
-globalThis.AbortController = AbortController;
-// import { wrapper } from "@/redux/store";     // НЕ РАБОТАЕТ С ТАКИМ IMPORT
-const { wrapper } = require("../redux/store"); //  ТОЛЬКО ТАКОЙ IMPORT И ТОЛЬКО  после AbortController
+// const MyApp = ({ Component, ...rest }) => {
 
-const MyApp = ({ Component, ...rest }) => {
-  
-  // const { store, props } = wrapper.useWrappedStore(rest);
-  const result = wrapper.useWrappedStore(rest);
+//   const { store, props } = wrapper.useWrappedStore(rest);
 
-  const getLayout = Component.getLayout || ((page) => page);
+//   const getLayout = Component.getLayout || ((page) => page);
 
-  return (
-    <Provider store={result.store}>
-      <GlobalLayout>
-        {getLayout(<Component {...result.props.pageProps} />)}
-      </GlobalLayout>
-      <GlobalStyles />
-    </Provider>
-  );
-};
+//   return (
+//     <Provider store={store}>
+//       <GlobalLayout>
+//         {getLayout(<Component {...props.pageProps} />)}
+//       </GlobalLayout>
+//       <GlobalStyles />
+//     </Provider>
+//   );
+// };
 
-// MyApp.getInitialProps = wrapper.getInitialAppProps(
-//   (store) => async (appCtx) => {
-//     // console.log("appCtx:========================", appCtx.Component);
-
-//     // const propData = appCtx.Component.getInitialProps
-//     // ? await appCtx.Component.getInitialProps({...appCtx.ctx, store}) 
-//     // : {}
-//     // console.log("propData:", propData);
-    
-//     // store.dispatch(setInitialCounter(10));
-
-//     // const ddd = await fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur');
-//     // const data = await ddd.json()
-//     // console.log("data:========================", data);
-
-//     const childrenGip = await App.getInitialProps(appCtx);
-
-//     return {
-//       pageProps: {
-//         ...childrenGip.pageProps,
-//         id: 42,
-//       },
-//     };
-
-    
-//   }
-// );
-
-export default MyApp;
+// export default MyApp;
 
 //=============================================================
 
+import App from "next/app";
 // import "../styles/globals.css";
-// import App from "next/app";
 // import { wrapper } from "@/redux/store";
+import GlobalLayout from "@/components/GlobalLayout";
+import { GlobalStyles } from "@/styles/globalStyles";
+import AbortController from "abort-controller";
 
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
+globalThis.AbortController = AbortController;
+const { wrapper } = require("../redux/store");
+
+function MyApp({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <>
+      <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default wrapper.withRedux(MyApp);
 
 // MyApp.getInitialProps = wrapper.getInitialAppProps(
 //   (store) => async (appCtx) => {
@@ -82,5 +64,3 @@ export default MyApp;
 //     };
 //   }
 // );
-
-// export default wrapper.withRedux(MyApp);
