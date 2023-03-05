9feb250d87c3cf3b768f86c1a150e961950c3df0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-05 20:05:14 +0200

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,27 +1,49 @@
 import { logIn } from "@/redux/auth/authSlice";
+import { wrapper } from "@/redux/store";
+import {
+  getRunningMutationsThunk,
+  getRunningQueriesThunk,
+  userApi,
+  userLogin,
+  useUserLoginMutation,
+} from "@/redux/walletApiService/userApi";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 
+export const getStaticProps = wrapper.getStaticProps((store) => async () => {
+  
+  const credentials = { email: "user100@mail.com", password: "a123456" };
+  store.dispatch(userLogin.initiate(credentials));
+  const [result] = await Promise.all(store.dispatch(getRunningMutationsThunk()));
+  // console.log("getStaticProps  result:", result);
+  // console.log("getStaticProps  store:", store.getState().auth);
+});
+
 const LoginPage = () => {
   const dispatch = useDispatch();
-  //   const {isLoggedIn} = useSelector(st => st.auth)
-  //   console.log("LoginPage  isLoggIn", isLoggedIn);
+  const { isLoggedIn, user } = useSelector((st) => st.auth);
+  // console.log("LoginPage  user:", user);
 
-  const onLogin = () => {
-    dispatch(logIn());
+  const [userLoginRTKQ] = useUserLoginMutation();
 
-    document.cookie = 'goit=djon4292; max-age=60'
+  const onLogin = async () => {
+    const credentials = {
+      email: "user100@mail.com",
+      password: "a123456",
+    };
 
-    const cookieValue = document.cookie
-  .split('; ')
-  .find((row) => row.startsWith('goit='))
-  ?.split('=')[1];
-  
-  console.log("LoginPage  cookieValue", cookieValue);
+    const currentUser = await userLoginRTKQ(credentials);
+    // console.log("onLogin  currentUser:", currentUser.data.token);
+
+    // dispatch(logIn());
+
+    // document.cookie = "goit=djon4292; max-age=60";
+    // const cookieValue = document.cookie
+    //   .split("; ")
+    //   .find((row) => row.startsWith("goit="))
+    //   ?.split("=")[1];
   };
 
-  
-  
   return (
     <>
       <h1>Login Page</h1>
