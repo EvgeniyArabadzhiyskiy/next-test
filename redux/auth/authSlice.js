d0042787885fc7d74c4a4210e633bac3c4faea16
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-05 20:05:22 +0200

    Update authSlice.js

--- redux/auth/authSlice.js
+++ redux/auth/authSlice.js
@@ -1,8 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { HYDRATE } from "next-redux-wrapper";
+import { userApi } from "../walletApiService/userApi";
 
 const authSlice = createSlice({
   name: "authSlice",
+
   initialState: {
+    user: { firstName: null, email: null, balance: 0 },
+    token: global.localStorage?.getItem("token") || null,
     isLoggedIn: false,
   },
 
@@ -14,6 +19,49 @@ const authSlice = createSlice({
     logOut: (state, action) => {
       state.isLoggedIn = false;
     },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(HYDRATE, (state, action) => {
+        let nextState = {
+          ...state,
+          ...action.payload.auth,
+        };
+
+        if (state.token) {
+          nextState = state;
+        }
+
+        return nextState;
+      })
+      .addMatcher(
+        userApi.endpoints.userLogin.matchFulfilled,
+        (state, action) => {
+          state.user = action.payload.user;
+          state.token = action.payload.token;
+          state.isLoggedIn = true;
+
+          // window.localStorage.setItem("token", action.payload.token);
+        }
+      )
+      .addMatcher(
+        userApi.endpoints.userLogout.matchFulfilled,
+        (state, action) => {
+          state.user = { name: null, email: null, balance: 0 };
+          state.token = null;
+          state.isLoggedIn = false;
+
+          // window.localStorage.setItem("token", "");
+        }
+      )
+      .addMatcher(
+        userApi.endpoints.userRefresh.matchFulfilled,
+        (state, action) => {
+          state.user = action.payload;
+          state.isLoggedIn = true;
+        }
+      );
   },
 });
 
