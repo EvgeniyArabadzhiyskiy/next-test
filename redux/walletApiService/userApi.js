b285b8d1963ded4d00a48cb719257f2e9fd53730
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-07 22:33:52 +0200

    Create userApi.js

---
+++ redux/walletApiService/userApi.js
@@ -0,0 +1,61 @@
+import { HYDRATE } from "next-redux-wrapper";
+import { baseQuery } from "./baseQuery";
+import { createApi } from "@reduxjs/toolkit/dist/query/react";
+import {
+  USER_CURRENT,
+  USER_LOGIN,
+  USER_LOGOUT,
+  USER_REGISTER,
+} from "@/constants/apiPath";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery,
+  tagTypes: ["User"],
+
+  extractRehydrationInfo(action, { reducerPath }) {
+    if (action.type === HYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
+
+  endpoints: (builder) => ({
+    userRefresh: builder.query({
+      query: () => `${USER_CURRENT}`,
+
+      providesTags: ["User"],
+    }),
+
+    userRegistration: builder.mutation({
+      query: (user) => ({
+        url: `${USER_REGISTER}`,
+        method: "POST",
+        body: user,
+      }),
+
+      invalidatesTags: ["User"],
+    }),
+
+    userLogin: builder.mutation({
+      query: (user) => {
+        return ({ url: `${USER_LOGIN}`, method: "POST", body: user })
+      },
+
+      invalidatesTags: ["User"],
+    }),
+
+    userLogout: builder.mutation({
+      query: () => ({ url: `${USER_LOGOUT}`, method: "POST" }),
+    }),
+  }),
+});
+
+export const {
+  useUserRegistrationMutation,
+  useUserLoginMutation,
+  useUserLogoutMutation,
+  useUserRefreshQuery,
+  util: { getRunningQueriesThunk, getRunningMutationsThunk },
+} = userApi;
+
+export const { userLogin, userLogout } = userApi.endpoints;
