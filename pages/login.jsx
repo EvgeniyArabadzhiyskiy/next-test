993de2271fdd3aa4e76bf1af533cc65dbf50c723
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-10 22:31:19 +0200

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -11,8 +11,11 @@ import Link from "next/link";
 import { setCookie } from "nookies";
 import { useDispatch, useSelector } from "react-redux";
 
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+
 // export const getStaticProps = wrapper.getStaticProps((store) => async () => {
-  
+
 //   const credentials = { email: "user100@mail.com", password: "a123456" };
 //   store.dispatch(userLogin.initiate(credentials));
 //   const [result] = await Promise.all(store.dispatch(getRunningMutationsThunk()));
@@ -20,43 +23,70 @@ import { useDispatch, useSelector } from "react-redux";
 //   // console.log("getStaticProps  store:", store.getState().auth);
 // });
 
-const LoginPage = () => {
-  const dispatch = useDispatch();
-  const { isLoggedIn, user, token } = useSelector((st) => st.auth);
+// const LoginPage = () => {
+//   const { data: session } = useSession()
+//   console.log("LoginPage  session:", session);
 
-  const [userLoginRTKQ] = useUserLoginMutation();
+//   const [userLoginRTKQ] = useUserLoginMutation();
 
-  const onLogin = async () => {
-    const credentials = {
-      email: "user100@mail.com",
-      password: "a123456",
-    };
+//   const onLogin = async () => {
+//     const credentials = {
+//       email: "user100@mail.com",
+//       password: "a123456",
+//     };
 
-    const currentUser = await userLoginRTKQ(credentials);
+//     const currentUser = await userLoginRTKQ(credentials);
 
-    setCookie(null, 'authToken', `${currentUser.data.token}`, { maxAge: 30 * 24 * 60 * 60})
+//     setCookie(null, 'authToken', `${currentUser.data.token}`, { maxAge: 30 * 24 * 60 * 60})
 
-    // dispatch(logIn());
+//     // document.cookie = `authToken=${currentUser.data.token}; max-age=${30*24*60*60}`;
+//     // const cookieValue = document.cookie
+//     //   .split("; ")
+//     //   .find((row) => row.startsWith("goit="))
+//     //   ?.split("=")[1];
+//   };
 
-    // document.cookie = `authToken=${currentUser.data.token}; max-age=${30*24*60*60}`;
-    // const cookieValue = document.cookie
-    //   .split("; ")
-    //   .find((row) => row.startsWith("goit="))
-    //   ?.split("=")[1];
+//   return (
+//     <>
+//       <h1>Login Page</h1>
+
+//       <button type="button" onClick={onLogin}>
+//         LOGIN
+//       </button>
+
+//       <Link href="/">Go Home</Link>
+//     </>
+//   );
+// };
+
+// export default LoginPage;
+
+export default function LoginPage() {
+  const { data: session } = useSession();
+  // console.log("LoginPage  session:", session);
+
+  const router = useRouter();
+
+  const onSignIn = async () => {
+    await signIn('google', { callbackUrl: 'http://localhost:3000/blog' });
+    // await router.push("/");
   };
 
+ 
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        {/* <button onClick={() => signOut()}>Sign out</button> */}
+        <Link href="/server"> ← Back to home</Link>
+      </>
+    );
+  }
   return (
     <>
-      <h1>Login Page</h1>
-      {/* <h2>User {token || 'DEFAULT'}</h2> */}
-
-      <button type="button" onClick={onLogin}>
-        LOGIN
-      </button>
-
-      <Link href="/">Go Home</Link>
+      Not signed in <br />
+      <button onClick={onSignIn}>Sign in</button>
     </>
   );
-};
-
-export default LoginPage;
+}
