7d7fdb01eff7224bce702563b837fe10e8ff3e0d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-13 22:19:40 +0200

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -14,14 +14,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 
-// export const getStaticProps = wrapper.getStaticProps((store) => async () => {
+export const getStaticProps = wrapper.getStaticProps((store) => async () => {
 
-//   const credentials = { email: "user100@mail.com", password: "a123456" };
-//   store.dispatch(userLogin.initiate(credentials));
-//   const [result] = await Promise.all(store.dispatch(getRunningMutationsThunk()));
-//   // console.log("getStaticProps  result:", result);
-//   // console.log("getStaticProps  store:", store.getState().auth);
-// });
+  const credentials = { email: "user100@mail.com", password: "a123456" };
+  store.dispatch(userLogin.initiate(credentials));
+  const [result] = await Promise.all(store.dispatch(getRunningMutationsThunk()));
+  // console.log("getStaticProps  result:", result);
+  // console.log("getStaticProps  store:", store.getState().auth);
+});
 
 const LoginPage = () => {
   const [userLoginRTKQ] = useUserLoginMutation();
@@ -61,9 +61,11 @@ const LoginPage = () => {
 
 export default LoginPage;
 
+//===========================================================
+
 // export default function LoginPage() {
 //   const { data: session } = useSession();
-//   // console.log("LoginPage  session:", session);
+//   console.log("LoginPage  session:", session);
 
 //   const router = useRouter();
 
@@ -71,6 +73,7 @@ export default LoginPage;
 //     await signIn('google', { callbackUrl: 'http://localhost:3000/blog' });
 //     // await router.push("/");
 //   };
+
 
 //   if (session) {
 //     return (
@@ -88,3 +91,46 @@ export default LoginPage;
 //     </>
 //   );
 // }
+
+//============================================================
+
+// import { signIn } from "next-auth/react";
+// import { useRouter } from "next/router";
+// import React, { useState } from "react";
+
+
+// const LoginPage = () => {
+//   const [error, setError] = useState("");
+//   const router = useRouter();
+
+//   const callbackUrl = router.query?.callbackUrl ?? "/";
+//   console.log("LoginPage  callbackUrl:", callbackUrl);
+  
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const _target = e.target;
+//     const email = _target.email.value;
+//     const password = _target.password.value;
+//     const result = await signIn("credentials", {
+//       email,
+//       password,
+//       callbackUrl: 'http://localhost:3000/blog',
+//       // redirect: false,
+//     });
+//     if (result?.error) {
+//       setError(result.error);
+//     } else {
+//       router.push(callbackUrl);
+//     }
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <h1>Sign In</h1>
+//       {!!error && <p>{error}</p>}
+//       <input type="text" placeholder="email" name="email" />
+//       <input type="password" placeholder="password" name="password" />
+//       <button type="submit">Sign In</button>
+//     </form>
+//   );
+// };
+// export default LoginPage;
