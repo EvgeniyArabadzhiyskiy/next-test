9afc0faee89f1740fcec198a1b459d7acce5b069
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-15 22:02:27 +0200

    Update PrivateRoute.jsx

--- components/PrivateRoute.jsx
+++ components/PrivateRoute.jsx
@@ -4,33 +4,41 @@ const { useRouter } = require("next/router");
 const { useEffect } = require("react");
 const { useSelector, useDispatch } = require("react-redux");
 
-const PrivateRoute = ({protectedRoutes, children }) => {
+const PrivateRoute = ({ protectedRoutes, children }) => {
   const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((s) => s.auth);
+  // console.log("PrivateRoute  isLoggedIn:", isLoggedIn);
 
   const router = useRouter();
 
-  const isProtected = protectedRoutes.indexOf(router.pathname) !== -1
-  // const isProtected = protectedRoutes?.some((path) => router.pathname === path);
-  // console.log("PrivateRoute  isProtected:", isProtected);
+  // const isProtected = protectedRoutes.reduce((acc, item) => {
+  //   if (!!item.match(router.pathname)) acc = true;
 
-  useEffect(() => {
-    const token = window.localStorage.getItem("authToken");
-    dispatch(setToken(token));
-  }, [dispatch]);
+  //   return acc;
+  // }, false);
+
+  const isProtected = protectedRoutes.indexOf(router.pathname) !== -1;
+  // const isProtected = protectedRoutes?.some((path) => router.pathname === path);
 
   useEffect(() => {
     if (!isLoggedIn && isProtected) {
       router.push("/login");
-      // console.log("PROTECT");
+    }
+
+    if (isLoggedIn && router.pathname === "/login") {
+      router.push("/");
     }
   }, [isLoggedIn, isProtected, router]);
+
+  if (isLoggedIn && router.pathname === "/login") {
+    return <h1>Loading...</h1>;
+  }
 
   if (!isLoggedIn && isProtected) {
     return <h1>Loading...</h1>;
   }
 
-  return children
+  return children;
 };
 
 export default PrivateRoute;
