4f837adfab00a87bbd2306d317a34eddc4f560a6
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-17 19:53:07 +0200

    Update proba.jsx

--- pages/pokemons/proba.jsx
+++ pages/pokemons/proba.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { wrapper } from "@/redux/store";
-import { useEffect, useState } from "react";
+import { useEffect, createContext, useContext, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Counter from "@/components/Counter/Counter";
 
@@ -25,57 +25,57 @@ import axios from "axios";
 import { useServerRedirect } from "@/lib/useServerRedirect";
 import { parseCookies } from "nookies";
 
-// export const getServerSideProps = wrapper.getServerSideProps(
-//   (store) => async (context) => {
-//     const { authToken } = parseCookies(context);
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context) => {
+    const { authToken } = parseCookies(context);
 
-//     // const { isLoggedIn } = store.getState().auth;
-//     // console.log("isLoggedIn:", isLoggedIn);
+    // const { isLoggedIn } = store.getState().auth;
+    // console.log("isLoggedIn:", isLoggedIn);
 
-//     // if (!isLoggedIn) {
-//     //   return {
-//     //     redirect: {
-//     //       destination: "/login",
-//     //     },
-//     //   };
-//     // }
+    // if (!isLoggedIn) {
+    //   return {
+    //     redirect: {
+    //       destination: "/login",
+    //     },
+    //   };
+    // }
 
-//     // let authSetHeader = context.req.headers
-//     // console.log("getServerSideProps  authSetHeader:", authSetHeader);
+    // let authSetHeader = context.req.headers
+    // console.log("getServerSideProps  authSetHeader:", authSetHeader);
 
-//     //   const response = await fetch(
-//     //   `https://wallet-backend-xmk0.onrender.com/api/transactions?page=1&limit=5`, {
-//     //     headers: {
-//     //       ["authorization"]: authSetHeader
-//     //     }
-//     //   }
-//     // );
-//     //  const data = await response.json()
-//     //  console.log("getServerSideProps  response:", response?.data.userBalance);
+    //   const response = await fetch(
+    //   `https://wallet-backend-xmk0.onrender.com/api/transactions?page=1&limit=5`, {
+    //     headers: {
+    //       ["authorization"]: authSetHeader
+    //     }
+    //   }
+    // );
+    //  const data = await response.json()
+    //  console.log("getServerSideProps  response:", response?.data.userBalance);
 
-//     // let authSetHeader = context.req.headers["authorization"] || null
-//     // store.dispatch(setToken(authSetHeader))
+    // let authSetHeader = context.req.headers["authorization"] || null
+    // store.dispatch(setToken(authSetHeader))
 
-//     // const { token } = store.getState().auth;
-//     // console.log("++++++++++++++++++++++token+++++++++++++++++++++++++:", token);
+    // const { token } = store.getState().auth;
+    // console.log("++++++++++++++++++++++token+++++++++++++++++++++++++:", token);
 
-//     // if (authToken) {
-//     //   store.dispatch(setToken(authToken));
+    if (authToken) {
+      store.dispatch(setToken(authToken));
 
-//       const { pageNum } = store.getState().transactions;
-//       store.dispatch(getAllTransactions.initiate({ pageNum }));
-//       const [result] = await Promise.all(
-//         store.dispatch(getRunningQueriesThunk())
-//       );
-//       // console.log("result:", result);
-//       store.dispatch(setInitialTransactions(result.data || []));
-//     // }
+      const { pageNum } = store.getState().transactions;
+      store.dispatch(getAllTransactions.initiate({ pageNum }));
+      const [result] = await Promise.all(
+        store.dispatch(getRunningQueriesThunk())
+      );
+      // console.log("result:", result);
+      store.dispatch(setInitialTransactions(result.data || []));
+    }
 
-//     store.dispatch(setInitialCounter({ amount: 10, type: "start" }));
+    store.dispatch(setInitialCounter({ amount: 10, type: "start" }));
 
-//     return { props: {} };
-//   }
-// );
+    return { props: {} };
+  }
+);
 
 const TransactionsList = () => {
   const [isSkip, setIsSkip] = useState(true);
@@ -87,28 +87,6 @@ const TransactionsList = () => {
     { pageNum },
     { skip: isSkip }
   );
-
-  useEffect(() => {
-    (async () => {
-      // const response = await fetch(`https://pokeapi.co/api/v2/pokemon`, {
-      //   headers:
-      //   {
-      //     "DDDDDD": "KIWI"
-      //   }
-      // });
-      // const data = await response.json();
-      // console.log("getStaticProps  data", data);
-      // const response = await fetch(
-      //   `https://wallet-backend-xmk0.onrender.com/api/transactions?page=1&limit=5`, {
-      //     headers: {
-      //       ["authorization"]: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3ODM3MTc2MywiZXhwIjoxNjc5NTgxMzYzfQ.NwpniKvSo3I6-NO0KwIca2b9FGCHBn23XsbKRF4lDtE",
-      //     }
-      //   }
-      // );
-      // const data = await response.json()
-      // console.log("getServerSideProps  data:", data);
-    })();
-  });
 
   useEffect(() => {
     if (data.length === 0) return;
@@ -285,12 +263,12 @@ export default TransactionsList;
 
 //===========================================================
 
+// import axios from "axios";
 // import Head from "next/head";
 // import Link from "next/link";
 // import { useEffect, useState } from "react";
-// import { useDeleteTransactionMutation } from "@/redux/walletApi";
+// import { useDeleteTransactionMutation } from "@/redux/walletApiService/walletApi";
 
-// import axios from "axios";
 // const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3Nzk0NTcxOSwiZXhwIjoxNjc5MTU1MzE5fQ.QSy4e8Qtlmu4tKzK9-i5WfRUhDSrdGjdRx7Cnfb3sac`;
 // axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
@@ -373,3 +351,117 @@ export default TransactionsList;
 // export default TransactionsList;
 
 // ========================================================
+
+// const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY3Nzk0NTcxOSwiZXhwIjoxNjc5MTU1MzE5fQ.QSy4e8Qtlmu4tKzK9-i5WfRUhDSrdGjdRx7Cnfb3sac`;
+// axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+// const getTransact = async (pageNum) => {
+//   const { data } = await axios(
+//     `https://wallet-backend-xmk0.onrender.com/api/transactions?page=${pageNum}&limit=5`
+//   );
+//   return data;
+// };
+
+// const TransactionsContext = createContext();
+
+// const TransactionsProvider = ({ children }) => {
+//   const [isSkip, setIsSkip] = useState(true);
+//   const [transactions, setTransactions] = useState([]);
+//   const [pageNum, setPageNum] = useState(1);
+//   // console.log("TransactionsProvider  pageNum:", pageNum);
+
+//   useEffect(() => {
+//     if (isSkip) return;
+//     const fetchData = async () => {
+//       const  data  = await getTransact(pageNum);
+//       // console.log("fetchData  data:", data);
+//       // setTransactions(data?.transactions);
+//       setTransactions((prev) => [...prev, ...data?.transactions]);
+//     };
+
+//     fetchData();
+//   }, [isSkip, pageNum]);
+
+//   const handleNextPage = () => {
+//     setIsSkip(false)
+//     setPageNum(pageNum + 1);
+//   };
+
+//   return (
+//     <TransactionsContext.Provider
+//       value={{
+//         transactions,
+//         handleNextPage,
+//         setTransactions,
+//       }}
+//     >
+//       {children}
+//     </TransactionsContext.Provider>
+//   );
+// };
+
+// const TransactionsList = () => {
+//   // const [isSkip, setIsSkip] = useState(true);
+//   const { transactions, handleNextPage, setTransactions } = useContext(TransactionsContext)
+
+//   // useEffect(() => {
+//   //   (async () => {
+//   //     // const data = await getTransact(1)
+//   //     const res = await fetch(
+//   //       `https://wallet-backend-xmk0.onrender.com/api/transactions?page=${1}&limit=5`,{
+//   //         headers: {
+//   //           ["Authorization"]: `Bearer ${token}`,
+//   //         }
+//   //       }
+//   //     );
+
+//   //     const data = await res.json()
+//   //     // console.log("getTransact  data:", data);
+      
+//   //     // setResult(data?.transactions);
+//   //   })();
+//   // }, []);
+
+ 
+
+//   return (
+//     <>
+//       <Head>
+//         <title>Transactions</title>
+//         <meta name="description" content="Transactions List" />
+//       </Head>
+
+//       <div>
+//         <h1>Transactions</h1>
+//         <Link href="/"> ← Back to home</Link> <Link href="/blog">TO BLOG</Link>
+//         <button type="button" onClick={handleNextPage}>
+//           Next Page
+//         </button>
+//       </div>
+
+//       <Counter />
+
+//       <ul className="transactions-list">
+//         {transactions &&
+//           transactions.map((item) => {
+//             return (
+//               <li key={item._id}>
+//                 <span>{item.category}</span>
+//                 <button type="button">DELETE</button>
+//               </li>
+//             );
+//           })}
+//       </ul>
+//     </>
+//   );
+// };
+
+// const WrappedTransactionsList = () => (
+//   <TransactionsProvider>
+//     <TransactionsList />
+//   </TransactionsProvider>
+// )
+
+// export default WrappedTransactionsList
+
+// export default TransactionsList;
