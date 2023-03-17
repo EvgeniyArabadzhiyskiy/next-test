84810b1dcc3f8c04280c018a2100adfe62088449
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-03-17 19:53:00 +0200

    Update PrivateRoute.jsx

--- components/PrivateRoute.jsx
+++ components/PrivateRoute.jsx
@@ -6,24 +6,14 @@ const { useEffect } = require("react");
 const { useSelector, useDispatch } = require("react-redux");
 
 const PrivateRoute = ({ protectedRoutes, children }) => {
-  const dispatch = useDispatch();
-  const { isLoggedIn, token } = useSelector((s) => s.auth);
-  // console.log("PrivateRoute  isLoggedIn:", isLoggedIn);
-
+  // const dispatch = useDispatch();
   const router = useRouter();
-
-  // const isProtected = protectedRoutes.reduce((acc, item) => {
-  //   if (!!item.match(router.pathname)) acc = true;
-
-  //   return acc;
-  // }, false);
+  const { isLoggedIn, token } = useSelector((s) => s.auth);
 
   const isProtected = protectedRoutes.indexOf(router.pathname) !== -1;
   // const isProtected = protectedRoutes?.some((path) => router.pathname === path);
 
-  //  const { isError, isLoading } = useUserRefreshQuery(undefined, {
-  //   skip: !token,
-  // });
+  // const { isError, isLoading } = useUserRefreshQuery(undefined, { skip: !token });
 
   useEffect(() => {
     if (!isLoggedIn && isProtected) {
