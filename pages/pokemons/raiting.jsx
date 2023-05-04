af6d32c6d22c185bb846e4113117c9bf09053dad
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-04 15:20:40 +0300

    Update raiting.jsx

--- pages/pokemons/raiting.jsx
+++ pages/pokemons/raiting.jsx
@@ -1,30 +1,101 @@
-import Counter from "@/components/Counter/Counter";
-import PokemonPageLayout from "@/components/PokemonPageLayout";
-import Raiting from "@/components/Raiting";
-import { incrementCounter } from "@/redux/counter/counter";
 import { wrapper } from "@/redux/store";
-import { useSelector } from "react-redux";
+import { setNextPage, setPrevPage } from "@/redux/transactions-slice";
+import {
+  useGetAllTransactionsQuery,
+  getAllTransactions,
+  getRunningQueriesThunk,
+  walletApi,
+  useAddTransactionMutation,
+} from "@/redux/walletApiService/walletApi";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
 
-// export const getStaticProps = wrapper.getStaticProps(
-//   (store) => async () => {
+const transData = {
+  amount: 500,
+  category: "WODA",
+  typeOperation: "expense",
+  comment: "Fruits",
+  // date: "Wed Apr 05 2023 21:43:29 GMT+0300 (Восточная Европа, летнее время)",
+  // Wed Apr 05 2023 21:43:44 GMT+0300 //==15==
+  date: "Wed Apr 05 2023 21:41:36 GMT+0300 (Восточная Европа, летнее время)",
+  // date: new Date().toString(),
+};
 
-    
+export const getServerSideProps = wrapper.getServerSideProps(
+  ({ dispatch }) =>
+    async () => {
+      const res = await dispatch(getAllTransactions.initiate({ pageNum: 1 }));
 
-//     store.dispatch(incrementCounter());
+      // const [result] = await Promise.all(
+      //   dispatch(getRunningQueriesThunk())
+      // );
 
-//     return { props: {} };
-//   }
-// );
+      return { props: {} };
+    }
+);
 
 const RaitingPage = () => {
-  const { counter } = useSelector(state => state.counter);
-    // console.log("RaitingPage  counter:", counter);
-    return ( <><Raiting /><Counter /></> );
-}
- 
+  const dispatch = useDispatch();
+  const { transactions, pageNum } = useSelector((state) => state.transactions);
+
+  const [addTransactionRTKQ] = useAddTransactionMutation();
+
+  const ddd = walletApi.endpoints.getAllTransactions.useQuery({pageNum});
+  // console.log("RaitingPage  ddd:", ddd);
+  // const { data = [] } = useGetAllTransactionsQuery({ pageNum });
+
+  const onNextPage = () => {
+    dispatch(setNextPage());
+
+    // setIsSkip(false);
+  };
+
+  const onPrevPage = () => {
+    dispatch(setPrevPage());
+  };
+
+  const addTransaction = () => {
+    addTransactionRTKQ(transData);
+  };
+
+  return (
+    <>
+      <Link href="/"> ← Back to home</Link>
+
+      <button type="button" onClick={onNextPage}>
+        Next Page
+      </button>
+
+      <button type="button" disabled={pageNum <= 1} onClick={onPrevPage}>
+        Prev Page
+      </button>
+
+      <ul className="transactions-list">
+        {ddd.data &&
+          ddd.data.map((item) => {
+            return (
+              <li key={item._id}>
+                <span>{item.category}</span>
+                <button
+                  type="button"
+                  onClick={() => deleteTransaction(item._id)}
+                >
+                  DELETE
+                </button>
+              </li>
+            );
+          })}
+      </ul>
+
+      <button type="button" onClick={addTransaction}>
+        NEW TRANSACTION
+      </button>
+    </>
+  );
+};
+
 export default RaitingPage;
 
-RaitingPage.getLayout = function getLayout(page) {
-  return <PokemonPageLayout>{page}</PokemonPageLayout>;
-  
-}
+// RaitingPage.getLayout = function getLayout(page) {
+//   return <PokemonPageLayout>{page}</PokemonPageLayout>;
+// };
