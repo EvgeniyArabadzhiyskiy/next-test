7755344d77d070e37108628f3bab7c69ec95795e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-05 22:35:11 +0300

    Update PrivateRoute.jsx

--- components/PrivateRoute.jsx
+++ components/PrivateRoute.jsx
@@ -5,29 +5,57 @@ const { useRouter } = require("next/router");
 const { useEffect } = require("react");
 const { useSelector, useDispatch } = require("react-redux");
 
+// const PrivateRoute = ({ protectedRoutes, children }) => {
+//   // const dispatch = useDispatch();
+//   const router = useRouter();
+//   const { isLoggedIn, token } = useSelector((s) => s.auth);
+//   console.log("PrivateRoute  isLoggedIn:", isLoggedIn);
+
+//   const isProtected = protectedRoutes.indexOf(router.pathname) !== -1;
+//   // const isProtected = protectedRoutes?.some((path) => router.pathname === path);
+
+//   // const { isError, isLoading } = useUserRefreshQuery(undefined, { skip: !token });
+
+//   useEffect(() => {
+//     // if (!isLoggedIn && isProtected) {
+//     //   router.push("/login");
+//     // }
+
+//     // if (isLoggedIn && router.pathname === "/login") {
+//     //   router.push("/");
+//     // }
+
+//     if (!isLoggedIn && router.pathname === "/pokemons/raiting") {
+//       router.push("/login");
+//     }
+//   }, [isLoggedIn, router]);
+
+//   // if (isLoggedIn && router.pathname === "/login") {
+//   //   return <h1>Loading...</h1>;
+//   // }
+
+//   if (!isLoggedIn && router.pathname === "/pokemons/raiting") {
+//     return <h1>Loading...</h1>;
+//   }
+
+//   return children;
+// };
+
+// export default PrivateRoute;
+
+
+
 const PrivateRoute = ({ protectedRoutes, children }) => {
-  // const dispatch = useDispatch();
   const router = useRouter();
-  const { isLoggedIn, token } = useSelector((s) => s.auth);
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
   const isProtected = protectedRoutes.indexOf(router.pathname) !== -1;
-  // const isProtected = protectedRoutes?.some((path) => router.pathname === path);
-
-  // const { isError, isLoading } = useUserRefreshQuery(undefined, { skip: !token });
-
+ 
   useEffect(() => {
     if (!isLoggedIn && isProtected) {
       router.push("/login");
     }
-
-    if (isLoggedIn && router.pathname === "/login") {
-      router.push("/");
-    }
   }, [isLoggedIn, isProtected, router]);
-
-  if (isLoggedIn && router.pathname === "/login") {
-    return <h1>Loading...</h1>;
-  }
 
   if (!isLoggedIn && isProtected) {
     return <h1>Loading...</h1>;
