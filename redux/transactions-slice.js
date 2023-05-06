db0aa2951d4523483da0f828250b35523fed1629
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-06 22:01:49 +0300

    Update transactions-slice.js

--- redux/transactions-slice.js
+++ redux/transactions-slice.js
@@ -24,22 +24,33 @@ const transactionsSlice = createSlice({
     setInitialTransactions: (state, action) => {
       state.transactions = action.payload;
     },
+
+  
   },
 
   extraReducers: (builder) => {
     builder.addCase(HYDRATE, (state, action) => {
-      let nextState = {
-        ...state,
-        ...action.payload.transactions,
-      };
+    // console.log("builder.addCase  HYDRATE:", HYDRATE);
 
-      if (state.transactions.length > 0) {
-        nextState.transactions = state.transactions;
-        nextState.pageNum = state.pageNum;
-        // nextState = state
+
+      if (state.transactions.length === 0) {
+        // console.log('TRANSA');
+        state.transactions = action.payload.transactions.transactions
+        state.pageNum = action.payload.transactions.pageNum
       }
 
-      return nextState;
+
+      // let nextState = {
+      //   ...state,
+      //   ...action.payload.transactions,
+      // };
+
+      // if (state.transactions.length > 0) {
+      //   nextState.transactions = JSON.parse(JSON.stringify(state.transactions));
+      //   nextState.pageNum = state.pageNum;
+      // }
+
+      // return nextState;
     });
   },
 });
