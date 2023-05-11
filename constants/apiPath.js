4a668ebba0b06e72e47bf4a3a58df5e52647f30e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-11 15:27:22 +0300

    Update apiPath.js

--- constants/apiPath.js
+++ constants/apiPath.js
@@ -1,4 +1,5 @@
 export const BASE_URL = 'https://wallet-backend-xmk0.onrender.com/api';
+// export const BASE_URL = 'http://localhost:4001/api';
 
 export const USER_CURRENT = '/users/current';
 export const USER_REGISTER = '/users/register';
